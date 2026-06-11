57efca42435d9e267656e4a4b3d17e3c2aa223f6
Fillipe Machado <f.souzamachado30@gmail.com>
2026-06-11 11:23:16 -0300

    fix: bug

--- backend/src/middlewares/error.middleware.js
+++
@@ -1,9 +0,0 @@
-function errorMiddleware(err, req, res, next) {
-  console.error(err)
-
-  return res.status(500).json({
-    message: "Erro interno do servidor"
-  })
-}
-
-module.exports = errorMiddleware
